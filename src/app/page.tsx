ce69490d520e3024011b85ec3fe262eb5cd6639a
RafaelSimasS <rafaeldesousasimas@gmail.com>
2025-03-17 22:53:49 -0300

    feat: implement theme management and update global styles with new color variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,96 @@
-import Image from "next/image";
+"use client";
+import AddBookButton from "@/components/AddBookButton";
+import BookCard from "@/components/BookCard/BookCard";
+import Navbar from "@/components/NavBar/Navbar";
+import { useBooks } from "@/context/BookContext";
+import { useEffect, useState } from "react";
+import { toast, Toaster } from "sonner";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { books, error, isFetching, removeBook } = useBooks();
+  const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {
+    document.title = "Lazy Reader - Home";
+  }, []);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  if (isFetching) {
+    return (
+      <>
+        <Navbar />
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+          <div className="text-gray-500 dark:text-gray-300">
+            Loading books...
+          </div>
+        </main>
+      </>
+    );
+  }
+  if (error) {
+    return (
+      <>
+        <Navbar />
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 text-gray-500 dark:text-gray-300">
+          <div className="text-inherit dark:text-inherit">{error}</div>
+        </main>
+      </>
+    );
+  }
+  if (!(books.length > 0)) {
+    return (
+      <>
+        <Navbar />
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 text-gray-500 dark:text-gray-300">
+          <div className="text-inherit dark:text-inherit">
+            <div className="w-full max-w-xs md:max-w-sm lg:max-w-md text-inherit dark:text-inherit max-w text-center md:text-left">
+              <p>No books found.</p>
+              <AddBookButton />
+            </div>
+          </div>
+        </main>
+      </>
+    );
+  }
+
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const handleDelete = async (id: string) => {
+    try {
+      await removeBook(id);
+      toast.success("Book removed successfully.", {
+        description: "The book was removed from your library.",
+      });
+    } catch (err) {
+      console.error(err);
+      toast.error("Failed to remove book.", {
+        description: "There was an error while removing the book.",
+      });
+    }
+  };
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-start justify-start p-2 text-gray-500 dark:text-gray-300">
+        <div className="w-full p-4">
+          <h1 className="text-3xl font-bold mb-4">Minha Biblioteca</h1>
+          <div className="flex items-center justify-between">
+            <input
+              type="text"
+              placeholder="Search book by name..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="p-2 border rounded w-[30%]"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <AddBookButton />
+          </div>
         </div>
+        <div className="grid grid-cols-2 mt-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 2xl:grid-cols-8 gap-6">
+          {filteredBooks.map((book, index) => (
+            <BookCard book={book} handleDelete={handleDelete} key={index} />
+          ))}
+        </div>
+        <Toaster />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
